afd1643e34c04041f698402018c2a92bf1c57f27
Aditya Vyavahare <vvaditya23@icloud.com>
2023-10-02 23:45:45 +0530

    configured decrypt functionality

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
 def encrypt(plain_text, shift_amount):
   encrypted_string = ""
   for letter in plain_text:
@@ -14,5 +13,11 @@ def encrypt(plain_text, shift_amount):
     encrypted_string += encrypted_letter
   print(encrypted_string) 
 
-    
-encrypt(text, shift)
+def decrypt(encrypted_text, shift_amount):
+  decrypted_string = ""
+  for letter in encrypted_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    decrypted_letter = alphabet[new_position]
+    decrypted_string += decrypted_letter
+  print(decrypted_string)
