1049c96087f893485ac7d19adb417853c875b620
Aditya Vyavahare <vvaditya23@icloud.com>
2023-10-02 23:46:20 +0530

    Added condition to encrypt or decrypt text

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def encrypt(plain_text, shift_amount):
   encrypted_string = ""
@@ -21,3 +19,14 @@ def decrypt(encrypted_text, shift_amount):
     decrypted_letter = alphabet[new_position]
     decrypted_string += decrypted_letter
   print(decrypted_string)
+
+if direction == "encode":
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  encrypt(text, shift)
+elif direction == "decode":
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  decrypt(text, shift)
+else:
+  print("Inavlid input!")
